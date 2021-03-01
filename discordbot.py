75661f2beab6b2d226f8730ab25c5c77db8fe732
shintaronagamine <79719289+shintaronagamine@users.noreply.github.com>
2021-03-01 11:19:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,28 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+# 起動時に動作する処理
+@bot.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# メッセージ受信時に動作する処理
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    alert_channel = client.get_channel(785246159978364938)
+    if before.channel is None: 
+        msg = f'{member.name} が入室しました。'
+        await alert_channel.send(msg)
 
 
 bot.run(token)
