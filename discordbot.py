79017f5d33621f3409b35a751399cc570973ad9f
shintaronagamine <79719289+shintaronagamine@users.noreply.github.com>
2021-03-01 13:38:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def maryo(ctx):
+    await ctx.send("ごめんなさいね～")
 
 
 bot.run(token)
